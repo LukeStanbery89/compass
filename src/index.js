aec59335967e64ecd872614c7e32e7d8d2251d06
Luke Stanbery <lukestanbery89@gmail.com>
2022-08-18 15:03:59 -0500

    add some error handling

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const { baseIP } = require('./config/config.json');
 
 function getURL(service) {
     const serviceData = serviceMap[service];
+    if (!serviceData) {
+        throw Error('COMPASS ERROR: Invalid service identifier');
+    }
     const ipSuffix = deviceMap[serviceData.device]?.ipSuffix;
     return `${serviceData.protocol}://${baseIP}${ipSuffix}:${serviceData.port}${serviceData.path}`;
 }
