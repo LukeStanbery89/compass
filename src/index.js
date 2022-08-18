bbcebf1cfc919e6228d99d0ca4215fa770cf4494
Luke Stanbery <lukestanbery89@gmail.com>
2022-08-18 15:40:24 -0500

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 const deviceMap = require('./config/device-map.json');
 const serviceMap = require('./config/service-map.json');
-const { baseIP } = require('./config/config.json');
 
 function getURL(service) {
     const serviceData = serviceMap[service];
     if (!serviceData) {
         throw Error('COMPASS ERROR: Invalid service identifier');
     }
-    const ipSuffix = deviceMap[serviceData.device]?.ipSuffix;
-    return `${serviceData.protocol}://${baseIP}${ipSuffix}:${serviceData.port}${serviceData.path}`;
+    const ip = deviceMap[serviceData.device]?.ip;
+    return `${serviceData.protocol}://${ip}:${serviceData.port}${serviceData.path}`;
 }
 
 module.exports = {
